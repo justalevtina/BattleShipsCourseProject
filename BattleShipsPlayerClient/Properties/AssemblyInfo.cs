746e14b5fb35e425c803d189604c7ebb79349302
justalevtina <alevtinashkrobot@gmail.com>
2024-12-15 22:04:13 +0300

    фигня

--- BattleShipsPlayerClient/Properties/AssemblyInfo.cs
+++ BattleShipsPlayerClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mastermind_Solver_Client")]
+[assembly: AssemblyTitle("BattleShipsPlayerClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mastermind_Solver_Client")]
+[assembly: AssemblyProduct("BattleShipsPlayerClient")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- BattleShipsServer/Properties/AssemblyInfo.cs
+++ BattleShipsServer/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mastermind_Server")]
+[assembly: AssemblyTitle("BattleShipsServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mastermind_Server")]
+[assembly: AssemblyProduct("BattleShipsServer")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
